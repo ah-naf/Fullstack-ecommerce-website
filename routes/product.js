4c51d4ae8e825a414ae8d52c852e105caacc65e4
Ahnaf <sheikhahnafshifat@gmail.com>
2021-12-25 01:41:23 +0600

    add wishlist

--- routes/product.js
+++ routes/product.js
@@ -82,6 +82,7 @@ router.get('/fav', verify, async (req,res) => {
     )
     res.status(200).json(fav.rows)
   } catch (error) {
+    console.log(error)
     res.status(500).json({message : error})
   }
 })
